58e48b43b869cb66c68be67e03c4665d6ec7b017
Nora <82054380+heynoranora@users.noreply.github.com>
2021-04-07 15:13:25 -0400

    new site

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/cache-list.js');
 
-var cacheName = 'chirpy-20210407.1510';
+var cacheName = 'chirpy-20210407.1513';
 
 function isExcluded(url) {
   const regex = /(^http(s)?|^\/)/; /* the regex for CORS url or relative url */
